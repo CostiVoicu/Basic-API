f41832ac407e7152d2ecf07bf18e77a20359997f
Constantin Voicu <voicu.constantin@outlook.com>
2024-04-22 15:37:08 +0300

    Add entities

---
+++ Project.API/Project.Database/Entities/Store.cs
@@ -0,0 +1,9 @@
+﻿namespace Project.Database.Entities
+{
+    public class Store
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
