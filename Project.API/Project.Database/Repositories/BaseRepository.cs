308c3098c39d34d8018d05376f255f65c068d61a
Constantin Voicu <voicu.constantin@outlook.com>
2024-04-22 17:17:56 +0300

    Add repository for product

---
+++ Project.API/Project.Database/Repositories/BaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using Project.Database.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Database.Repositories
+{
+    public class BaseRepository
+    {
+        protected PetShopDbContext petShopDbContext { get; set; }
+
+        public BaseRepository(PetShopDbContext petShopDbContext)
+        {
+            this.petShopDbContext = petShopDbContext;
+        }
+
+        public void SaveChanges()
+        {
+            petShopDbContext.SaveChanges();
+        }
+    }
+}
---
+++ Project.API/Project.Database/Repositories/ProductsRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Database.Context;
+using Project.Database.Entities;
+using System.Threading.Tasks;
+
+namespace Project.Database.Repositories
+{
+    public class ProductRepository : BaseRepository
+    {
+        public ProductRepository(PetShopDbContext petShopDbContext) : base(petShopDbContext)
+        {
+        }
+
+        public Product GetById(int productId)
+        {
+            var result = petShopDbContext.Products
+                .Include(e => e.Store)
+
+                .Where(e => e.Id == productId)
+
+                .FirstOrDefault();
+
+            return result;
+        }
+    }
+}
