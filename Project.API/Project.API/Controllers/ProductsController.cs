5884a9f77ed4562cddb38ac22dbba2833115dc7d
Constantin Voicu <voicu.constantin@outlook.com>
2024-04-22 20:05:05 +0300

    Add ProductsController Add StartupConfig Change Program.cs

---
+++ Project.API/Project.API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Project.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Core.Controllers
+{
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        private ProductsService productsService { get; set; }
+
+        public ProductsController(
+            ProductsService productsService
+            )
+        {
+            this.productsService = productsService;
+        }
+
+        [HttpGet]
+        [Route("{productId}/get-details")]
+        public IActionResult GetProductDetails([FromRoute] int productId)
+        {
+            var result = productsService.GetProductDetails(productId);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Project.API/Project.API/StartupConfig.cs
@@ -0,0 +1,23 @@
+﻿using Project.Core.Services;
+using Project.Database.Context;
+using Project.Database.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Api
+{
+    public static class StartupConfig
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ProductsService>();
+        }
+
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddDbContext<PetShopDbContext>();
+            services.AddScoped<DbContext, PetShopDbContext>();
+
+            services.AddScoped<ProductsRepository>();
+        }
+    }
+}
