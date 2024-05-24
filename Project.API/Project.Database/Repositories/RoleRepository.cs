a3ea449c8815f1543510ff6049b42f7c0030d35f
Constantin Voicu <voicu.constantin@outlook.com>
2024-05-24 23:39:17 +0300

    Add Users and roles repositories

---
+++ Project.API/Project.Database/Repositories/RoleRepository.cs
@@ -0,0 +1,25 @@
+﻿using Project.Database.Context;
+using Project.Database.Entities;
+using Project.Infrastructure.Exceptions;
+
+namespace Project.Database.Repositories
+{
+    public class RoleRepository : BaseRepository
+    {
+        public RoleRepository(PetShopDbContext petShopDbContext) : base(petShopDbContext)
+        {
+        }
+
+        public Role GetRoleByName(string name)
+        {
+            var result = petShopDbContext.Roles
+                .Where(e => e.Name == name)
+                .FirstOrDefault();
+
+            if (result == null)
+                throw new ResourceMissingException("Role not found");
+
+            return result;
+        }
+    }
+}
